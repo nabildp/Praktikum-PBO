0e198b8675f4f0571657e0928106001b9acf4156
Nabil Delma Putra <nabildelmaputra08@gmail.com>
2026-07-07 16:53:11 +0700

    Add files via upload

---
+++ Pratikum12032026/src/main/java/com/mycompany/pratikum12032026/Pratikum12032026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pratikum12032026;
+
+/**
+ *
+ * @author Nabil
+ */
+public class Pratikum12032026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
